3baaf11dd53ab5b6b3b016f7925e2685bb2ade49
RaphaelMW <raphael@mundowap.com.br>
2022-10-03 20:47:09 -0300

    Upload Favicon

--- src/layout/Wrapper/index.tsx
+++ src/layout/Wrapper/index.tsx
@@ -1,6 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { WrapperConteiner } from "./styles";
 
-export function Wrapper({ children }) {
+interface Props {
+  children: React.ReactElement;
+}
+
+export function Wrapper({ children }: Props) {
   return <WrapperConteiner>{children}</WrapperConteiner>;
 }
